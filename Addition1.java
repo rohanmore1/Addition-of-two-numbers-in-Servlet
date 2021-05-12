1f31d138dd411b53bf080ba82d6aa6db32f7f77a
rohanmore1 <69668795+rohanmore1@users.noreply.github.com>
2021-05-12 09:47:10 +0530

    Add files via upload

---
+++ Addition1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Hp
+ */
+@WebServlet(urlPatterns = {"/Addition1"})
+public class Addition1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        int number1 = Integer.parseInt(request.getParameter("no1"));
+        int number2 = Integer.parseInt(request.getParameter("no2"));
+        
+        int totalsum= number1 + number2;
+        
+        out.println("The Total is =" +totalsum);
+    }
+}
+ 
+
+
